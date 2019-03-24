3bd4f883c83d2b9d65496a8c360738872f7192df
Allen F Partlow <allenfp@gmail.com>
2019-03-24 06:52:27 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -67,7 +67,7 @@ class Owner
   end
 
   def list_pets
-      "I have #{@pets[:fishes].count}2 fish, 3 dog(s), and 1 cat(s)."
+      "I have #{@pets[:fishes].count} fish, 3 dog(s), and 1 cat(s)."
   end
 
 end
